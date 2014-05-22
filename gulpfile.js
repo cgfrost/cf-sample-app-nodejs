034267a47309067af018f6e8939d6887ac538e26
Christopher Frost <cfrost@gopivotal.com>
2014-05-22 16:57:51 +0100

    Adding initial support for testing

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,35 @@ var gulp = require('gulp');
 // include plug-ins
 
 var clean = require('gulp-clean');
-var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var jslint = require('gulp-jslint');
 
 
-gulp.task('default', ['clean', 'jshint'], function () {
+gulp.task('default', ['clean', 'jshint', 'test'], function () {
 });
 
 gulp.task('test', function(){
-
+    gulp.src('test/**/*.js')
+        .pipe(mocha({
+            reporter: 'spec'
+        }))
+        .on("error", function (err) {
+            console.log(err.toString());
+            this.emit('end');
+        });
 });
 
 gulp.task('jshint', function () {
 	gulp.src(['./server.js', './lib/*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
+		.pipe(jslint({
+            nomen: true,
+            white: true,
+            errorsOnly: false
+        }))
+        .on("error", function (err) {
+            console.log(err.toString());
+            this.emit('end');
+        });
 });
 
 
